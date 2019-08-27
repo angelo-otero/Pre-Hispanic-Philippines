8840ab8cd49012f79237f1c2a783c11becb4fd37
Angelo Otero <otero.angelo20@gmail.com>
2019-08-26 20:59:11 -0400

    Add most image captions and fix most image hover issues

--- public/Javascript/jQuery.js
+++ public/Javascript/jQuery.js
@@ -1,11 +1,12 @@
 //jshint esversion:6
 
 $(document).ready(() => {
+// general image variables
   const img = $('.img');
   const imgContainer = $('.img-container');
   const imgCaption = $('.img-caption');
 
-  // highland variables
+// highland variables
   const aetaImage = $('#aetaImg');
   const aetaButton = $('#aetaButton');
   const aetaGallery = $('#Aeta .highland-people-gallery');
@@ -18,26 +19,46 @@ $(document).ready(() => {
   const ilongotThumbnail = $('#IlongotGallery img');
   const highlandThumbnail = $('.img-thumbnail');
 
-
+// hides gallery for Aeta and Ilongot groups
   $('.highland-people-gallery').hide();
+// hides the caption for all images
   imgCaption.hide();
 
-  $(function () {
-  $('[data-toggle="popover"]').popover();
-});
+// initialize popovers in bootstrap
+  $(function() {
+    $('[data-toggle="popover"]').popover();
+  });
 
+// zooms in on image and shows the caption and color
+// background of the image when the mouse hovers,
+// then hides it when no longer hovering
   imgContainer.on('mouseenter', function() {
     imgCaption.fadeIn(500);
-    img.css({'opacity': '.3', 'cursor': 'pointer', 'transform': 'scale(1.2)'});
-  }).on('mouseleave', function(){
+    img.css({
+      'opacity': '.3',
+      'cursor': 'pointer',
+      'transform': 'scale(1.2)'
+    });
+  }).on('mouseleave', function() {
     imgCaption.fadeOut(500);
-    img.css({'opacity': '1', 'cursor': 'default', 'transform': 'scale(1)'});
+    img.css({
+      'opacity': '1',
+      'cursor': 'default',
+      'transform': 'scale(1)'
+    });
   });
 
+// ensures the image color overlay remains when
+// the mouse hovers over the caption
   imgCaption.on('mouseenter', function() {
-    img.css({'opacity': '.3', 'transform': 'scale(1.2)'});
+    img.css({
+      'opacity': '.3',
+      'transform': 'scale(1.2)'
+    });
   });
 
+// checks to see if the gallery is currently hidden
+// based on button text, then toggles it on click
   aetaButton.on('click', function() {
     if (aetaButton.text() === 'View Gallery') {
       aetaInfo.fadeOut(1000);
@@ -50,6 +71,8 @@ $(document).ready(() => {
     }
   });
 
+  // checks to see if the gallery is currently hidden
+  // based on button text, then toggles it on click
   ilongotButton.on('click', function() {
     if (ilongotButton.text() === 'View Gallery') {
       ilongotInfo.fadeOut(1000);
@@ -62,26 +85,30 @@ $(document).ready(() => {
     }
   });
 
+// changes the mouse icon to
+// a pointer when hover over the img
   highlandThumbnail.on('mouseenter', () => {
     highlandThumbnail.css({
       'cursor': 'pointer'
     });
   });
 
+// changes the large image on the left
+// based on the image thumbnail clicked
   aetaThumbnail.on('click', function() {
     const imgSrc = $(this).attr('src');
     aetaImage.fadeOut(1000, function() {
       aetaImage.attr('src', imgSrc).fadeIn(1000);
     });
-    console.log('aeta');
   });
 
+  // changes the large image on the left
+  // based on the image thumbnail clicked
   ilongotThumbnail.on('click', function() {
     const imgSrc = $(this).attr('src');
     ilongotImage.fadeOut(1000, function() {
       ilongotImage.attr('src', imgSrc).fadeIn(1000);
     });
-    console.log('Ilongot');
   });
 
 });
