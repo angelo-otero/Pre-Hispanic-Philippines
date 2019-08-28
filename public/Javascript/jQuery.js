c9f4c492f5355c4e26b9c4bfdea0b16ec21c623d
Angelo Otero <otero.angelo20@gmail.com>
2019-08-27 21:21:24 -0400

    Fix image hover issues. Start adding italicized class for popovers

--- public/Javascript/jQuery.js
+++ public/Javascript/jQuery.js
@@ -1,12 +1,12 @@
 //jshint esversion:6
 
 $(document).ready(() => {
-// general image variables
+  // general image variables
   const img = $('.img');
   const imgContainer = $('.img-container');
   const imgCaption = $('.img-caption');
 
-// highland variables
+  // highland variables
   const aetaImage = $('#aetaImg');
   const aetaButton = $('#aetaButton');
   const aetaGallery = $('#Aeta .highland-people-gallery');
@@ -19,46 +19,47 @@ $(document).ready(() => {
   const ilongotThumbnail = $('#IlongotGallery img');
   const highlandThumbnail = $('.img-thumbnail');
 
-// hides gallery for Aeta and Ilongot groups
+  // hides gallery for Aeta and Ilongot groups
   $('.highland-people-gallery').hide();
-// hides the caption for all images
+  // hides the caption for all images
   imgCaption.hide();
 
-// initialize popovers in bootstrap
+  // initialize popovers in bootstrap
   $(function() {
     $('[data-toggle="popover"]').popover();
   });
 
-// zooms in on image and shows the caption and color
-// background of the image when the mouse hovers,
-// then hides it when no longer hovering
+  // zooms in on image and shows the caption and color
+  // background of the image when the mouse hovers,
+  // then hides it when no longer hovering
   imgContainer.on('mouseenter', function() {
-    imgCaption.fadeIn(500);
-    img.css({
+    $(this).find(imgCaption).fadeIn(500);
+    $(this).find(img).css({
       'opacity': '.3',
       'cursor': 'pointer',
       'transform': 'scale(1.2)'
     });
   }).on('mouseleave', function() {
-    imgCaption.fadeOut(500);
-    img.css({
+    $(this).find(imgCaption).fadeOut(500);
+    $(this).find(img).css({
       'opacity': '1',
       'cursor': 'default',
       'transform': 'scale(1)'
     });
   });
 
-// ensures the image color overlay remains when
-// the mouse hovers over the caption
+  // ensures the image color overlay remains when
+  // the mouse hovers over the caption
   imgCaption.on('mouseenter', function() {
-    img.css({
+    $(this).find(img).css({
       'opacity': '.3',
-      'transform': 'scale(1.2)'
+      'transform': 'scale(1.2)',
+      'cursor': 'pointer'
     });
   });
 
-// checks to see if the gallery is currently hidden
-// based on button text, then toggles it on click
+  // checks to see if the gallery is currently hidden
+  // based on button text, then toggles it on click
   aetaButton.on('click', function() {
     if (aetaButton.text() === 'View Gallery') {
       aetaInfo.fadeOut(1000);
@@ -85,30 +86,78 @@ $(document).ready(() => {
     }
   });
 
-// changes the mouse icon to
-// a pointer when hover over the img
+  // changes the mouse icon to
+  // a pointer when hover over the img
   highlandThumbnail.on('mouseenter', () => {
     highlandThumbnail.css({
       'cursor': 'pointer'
     });
   });
 
-// changes the large image on the left
-// based on the image thumbnail clicked
+  // changes the large image on the left
+  // based on the image thumbnail clicked
   aetaThumbnail.on('click', function() {
     const imgSrc = $(this).attr('src');
+    let aetaTitle = $('.aeta-title');
+    let aetaSource = $('.aeta-source');
     aetaImage.fadeOut(1000, function() {
       aetaImage.attr('src', imgSrc).fadeIn(1000);
     });
+    // changes the image caption based
+    // on the src of thumbnail clicked
+    switch (imgSrc) {
+      case 'Images/Aeta1.jpg':
+        aetaTitle.text('Aeta Women and their Children ca. 1890 - ca. 1923');
+        aetaSource.text('PD-US-Expired');
+        break;
+      case 'Images/Aeta2.jpg':
+        aetaTitle.text('Aeta Group 1902');
+        aetaSource.text('PD-US-Expired');
+        break;
+      case 'Images/Aeta3.jpg':
+        aetaTitle.text('Aeta Man and Woman 1885');
+        aetaSource.html('<p>Photo By: <a href="https://commons.wikimedia.org/wiki/File:Bosquejo_del_archipi%C3%A9lago_filipino,_1885_%22Negritos_o_Aetas%22_(3817431370).jpg" target="_blank">Antonio Machado</a></p>');
+        break;
+      case 'Images/Aeta4.jpg':
+        aetaTitle.text('Aeta Girl 1902');
+        aetaSource.text('PD-US-Expired');
+        break;
+      default:
+
+    }
   });
 
   // changes the large image on the left
   // based on the image thumbnail clicked
   ilongotThumbnail.on('click', function() {
     const imgSrc = $(this).attr('src');
+    let ilongotTitle = $('.ilongot-title');
+    let ilongotSource = $('.ilongot-source');
     ilongotImage.fadeOut(1000, function() {
       ilongotImage.attr('src', imgSrc).fadeIn(1000);
     });
+    // changes the image caption based
+    // on the src of thumbnail clicked
+    switch (imgSrc) {
+      case 'Images/Ilongot1.jpg':
+        ilongotTitle.text('Ilongot Hunting Party 1910');
+        ilongotSource.text('PD-US-Expired');
+        break;
+      case 'Images/Ilongot2.jpg':
+        ilongotTitle.text('Ilongot Rice Farmers 1910');
+        ilongotSource.text('PD-US-Expired');
+        break;
+      case 'Images/Ilongot3.jpg':
+        ilongotTitle.text('Ilongot Men and Women 1910');
+        ilongotSource.text('PD-US-Expired');
+        break;
+      case 'Images/Ilongot4.jpg':
+        ilongotTitle.text('Ilongot Man');
+        ilongotSource.text('PD-US-Expired');
+        break;
+      default:
+
+    }
   });
 
 });
