de243219cc50d542859aff66820a5d6e854e87c2
Angelo Otero <otero.angelo20@gmail.com>
2020-04-28 15:52:25 -0400

    add Procfile and heroku setup

--- app.js
+++ app.js
@@ -278,6 +278,11 @@ app.get("/further-reading", function(req, res) {
   res.render("further-reading");
 });
 
-app.listen('3000', function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if(port == null || port ==""){
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
